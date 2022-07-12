8320021678ca7552b7a6bb2e5790e58130289731
HLW\Hlw <hlw@snoppa.com>
2022-07-12 18:39:16 +0800

    three

--- app/src/main/java/com/example/myapplication/thirdActivity.java
+++ app/src/main/java/com/example/myapplication/thirdActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -11,7 +13,14 @@ public class thirdActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third_layout);
     }
+    //文件存储
     private void save(){
 
+    }
+    //sharedPreferences存储
+    private void spSave(){
+        SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+        editor.putString("name","zhangsan");
+        editor.apply();//数据提交
     }
 }
